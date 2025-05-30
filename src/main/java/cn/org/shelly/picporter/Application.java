685fb37792a062f2f107326860151dfab0754c8a
shelly1227 <40505282@qq.com>
2025-05-30 15:57:25 +0800

    feat: initialize project structure

---
+++ src/main/java/cn/org/shelly/picporter/Application.java
@@ -0,0 +1,10 @@
+package cn.org.shelly.picporter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(Application.class, args);
+    }
+}
